4e9bdfb57847a612c54c0f584f1f110ae5ca856c
PassionDev206 <bluewolf98219@gmail.com>
2024-07-07 04:42:33 -0700

    Add modules to confirm that I can get a profit with several strategy

--- confirm/everytime.py
+++ confirm/everytime.py
@@ -5,41 +5,34 @@ if __name__ == "__main__":
 		history = json.load(file)
 	profit_cnt = 0
 	loss_cnt = 0
+	dif = []
 	profit_range = input("Profit range:")
 	profit_range = float(profit_range)
 	temp = 0
-	total_profit = 0
-	high_crash = 1
-	high_bet_flag = False
 	cnt = 0
+	total_profit = 0
 	daily_profit = []
 	temp_profit = 0
 	for i in range(1000000):
 		cnt += 1
-		if history[i] < (1 + profit_range):
-			if high_bet_flag == True:
-				total_profit -= high_crash
-			else:
-				total_profit -= 1
+		if history[i] < 1 + profit_range:
+			total_profit -= 1
 			loss_cnt += 1
-			high_bet_flag = True
+			temp += 1
 		else:
 			profit_cnt += 1
-			if high_bet_flag == True:
-				temp += 1
-				total_profit += high_crash * profit_range
-				if temp == 1:
-					high_bet_flag = False
-					temp = 0
-			else:
-				total_profit += profit_range
+			total_profit += profit_range
+			if temp > 0:
+				dif.append(temp)
+				temp = 0
 		if cnt % 3000 == 0:
 			daily_profit.append(total_profit - temp_profit)
 			temp_profit = total_profit
 
-	with open("daily_profit1.json", "w") as file:
+	with open("difference.json", "w") as file:
+		json.dump(dif, file, indent=2)
+	with open("daily_profit.json", "w") as file:
 		json.dump(daily_profit, file, indent=2)
 	print("Loss count: ", loss_cnt)
 	print("Profit count:", profit_cnt)
-	# total_profit = profit_range * profit_cnt - loss_cnt
 	print("Total profit: ", total_profit)
